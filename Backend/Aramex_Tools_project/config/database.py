fcda9b4a86305e5a105ac1ef94f81a0c84242b92
ZeyadMohamedAyoub <108410156+ZeyadMohamedAyoub@users.noreply.github.com>
2024-11-04 14:46:29 +0200

    removed .zip file

---
+++ Backend/Aramex_Tools_project/config/database.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+
+
+client = MongoClient("mongodb+srv://mohamedsolimanfcai:Ashraf_123@aramex.6k45j.mongodb.net/?retryWrites=true&w=majority&appName=Aramex")
+
+db = client.user_db
+
+collection_name = db["user_collection"]
---
+++ Backend/Aramex_Tools_project/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app=FastAPI()
+
+from routes.route import router
+
+app.include_router(router)
+
+origins = [
+    "http://localhost:4200",  
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://mohamedsolimanfcai:Ashraf_123@aramex.6k45j.mongodb.net/?retryWrites=true&w=majority&appName=Aramex"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
